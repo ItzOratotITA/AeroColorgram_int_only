14ccac7486b331b2770e3a5d34b0da500530ddf4
ItzOratotITA <paride.totaro.2014@gmail.com>
2024-11-19 15:29:00 +0100

    Finally I did it!

--- aerocolorgram.py
+++ aerocolorgram.py
@@ -5,28 +5,46 @@ console = Console()
 printconsole = console.print
 cin = console.input
 
-supported_colors = ("bright_red", "bright_green", "bright_yellow", "bright_blue", "bright_magenta")
+supported_colors = (
+    "bright_red",
+    "bright_green",
+    "bright_yellow",
+    "bright_blue",
+    "bright_magenta",
+)
 
-printconsole("""[bold]Welcome to AeroColorgram!
-Supported colors:[/bold]""")
+printconsole("[bold]Welcome to AeroColorgram!\n" "Supported colors:[/bold]")
 
 for color in supported_colors:
     printconsole(f"[{color}]{color}[/{color}]")
 
-printconsole("[bold]For more colors visit https://rich.readthedocs.io/en/stable/appendix/colors.html[/bold]")
+printconsole(
+    "[bold]For more colors visit https://rich.readthedocs.io/en/stable/appendix/colors.html[/bold]"
+)
 
-items_num = range(1, int(cin("How much items do you want to include? ")) + 1 )
+items_num = int(cin("How much items do you want to include? "))
 items = []
 items_colors = []
 
-for num in items_num:
-    items.append(int(cin(f"Insert percent for item {num} (don't type '%'): ")))
-    items_colors.append(cin(f"Insert color for item {num}: "))
+for idx in range(items_num):
+    items.append(int(cin(f"Insert percent for item {idx} (don't type '%'): ")))
+    items_colors.append(cin(f"Insert color for item {idx}: "))
 
 printconsole("What i understood:")
 items_length = list(range(len(items)))
 
-printconsole(items_length) #debug
+printconsole(items_length)  # debug
 
-for num in items_length:
-    printconsole(f"Percent: {items[num]}% Color: [{items_colors[num]}]{items_colors[num]}[/{items_colors[num]}]")
+for idx in items_length:
+    printconsole(
+        f"Percent: {items[idx]}% Color: [{items_colors[idx]}]{items_colors[idx]}[/{items_colors[idx]}]"
+    )
+
+item_data = list(zip(items, items_colors))
+listona = []
+for p, c in item_data:
+    for j in range(p):
+        listona.append(f"[{c}]■[/{c}]")
+
+for i in range(0, 100, 10):
+    printconsole("".join(listona[i : i + 10]))
